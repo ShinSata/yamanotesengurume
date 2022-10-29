fbf16c20de20b5956647955b152086fc1e74c26a
ShinSata <shin.st.12066713@gmail.com>
2022-10-29 14:13:27 +0900

    ○○.index.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource) 
     user_path(current_user.id)
   end
+  def after_sign_in_path_for(resource)
+    root_path 
+  end
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
